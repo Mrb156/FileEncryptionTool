84c487566cadbe1b9f4fcbe7084ee2e8abd1a3f9
Morvai Barna <morvaibarna@gmail.com>
2023-03-23 18:02:31 +0100

    edited exec.sh

--- src/exec.sh
+++ src/exec.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#export PATH="$DIR:$PATH"
-echo export PATH=$DIR:$PATH >>.bashrc
-mv encryt_file.py encryption-tool
+echo export PATH=$DIR:$PATH >> $HOME/.bashrc
+mv encrypt_file.py encryption-tool
 chmod +x encryption-tool
