67fb3266efb78e0049685ef9599346cdda4717a8
Morvai Barna <morvaibarna@gmail.com>
2023-03-23 18:49:39 +0100

    final path function and added package install

--- src/exec.sh
+++ src/exec.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+# Set global environment variable for the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
 mv encrypt_file.py encryption-tool
 chmod +x encryption-tool
+
+# Check if pip is installed, and install it if necessary
+if ! command -v pip &> /dev/null; then
+    echo "Installing pip..."
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
+fi
+
+# Install packages from requirements.txt
+echo "Installing required packages..."
+pip install -r requirements.txt
