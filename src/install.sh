9d4b209a15105b98fdae46ab7e16c66f0bfff755
Morvai Barna <morvaibarna@gmail.com>
2023-03-27 10:00:28 +0200

    renamed

---
+++ src/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Set color for commands
+RED="\033[31m"
+NORMAL="\033[0;39m"
+
+# Set global environment variable for the script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
+mv encrypt_file.py encryption-tool
+chmod +x encryption-tool
+
+# Check if pip is installed, and install it if necessary
+if ! command -v pip &> /dev/null; then
+    echo -e $RED "Installing pip..." $NORMAL
+    sudo apt-get update
+    sudo apt-get install -y python3-pip
+fi
+
+# Install packages from requirements.txt
+echo -e $RED "Installing required packages..." $NORMAL
+pip install -r requirements.txt
