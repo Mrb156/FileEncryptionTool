1fe7f7b845ca0d61130edae7704512d00eb72a3d
Morvai Barna <morvaibarna@gmail.com>
2023-04-08 16:24:14 +0200

    removed naming

--- src/install.sh
+++ src/install.sh
@@ -7,7 +7,7 @@ NORMAL="\033[0;39m"
 # Set global environment variable for the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
-mv encrypt_file.py encryption-tool
+#mv encrypt_file.py encryption-tool
 chmod +x encryption-tool
 
 mv zipper.py compress-tool
