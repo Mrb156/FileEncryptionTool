26007f6d3bb44b54f7a7d011132459d1c099f249
Morvai Barna <morvaibarna@gmail.com>
2023-04-21 16:29:44 +0200

    added masking

--- src/encrypt_file.py
+++ src/encrypt_file.py
@@ -3,26 +3,30 @@
 import argparse
 import pyAesCrypt
 import os
+import maskpass
 
-def encryptFile(input_file, output_file, password):
+def encryptFile(input_file, output_file):
+    password = maskpass.askpass(prompt = 'Password: ', mask="*")
     pyAesCrypt.encryptFile(input_file, output_file, password)
     os.remove(input_file)
     
-def decryptFile(input_file, output_file, password):
+def decryptFile(input_file, output_file):
+    password = maskpass.askpass(prompt = 'Password: ', mask="*")
     pyAesCrypt.decryptFile(input_file, output_file, password)
     os.remove(input_file)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Encrypt or decrypt a file.')
-    parser.add_argument('type', type=str, help='Encrypt or decrypt.')
-    parser.add_argument('input_file', type=str, help='The input file to encrypt or decrypt.')
-    parser.add_argument('output_file', type=str, help='The output file to encrypt or decrypt.')
-    #parser.add_argument('-a','--algorithm', type=str,choices=['AES','blowfish'], default='AES', help='The encryption algorithm to use. AES or blowfish')
-    parser.add_argument('password', type=str, help='Password for encrypt or decrypt.')
-    args = parser.parse_args()
+    parser.add_argument('-t','--type', type=str,choices=['en','de'], help='Encrypt or decrypt.')
+    parser.add_argument('-in','--input_file', type=str, help='The input file to encrypt or decrypt.')
+    parser.add_argument('-out', '--output_file', type=str, help='The output file to encrypt or decrypt.')
 
-    if args.type == "en":
-        encryptFile(args.input_file, args.output_file, args.password)
-    elif args.type == "de":
-        decryptFile(args.input_file, args.output_file, args.password)
+    args = parser.parse_args()
+    try:
+        if args.type == "en":
+            encryptFile(args.input_file, args.output_file)
+        elif args.type == "de":
+            decryptFile(args.input_file, args.output_file)
+    except:
+        print('An error occured!')
 
