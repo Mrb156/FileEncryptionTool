ee3e47ce4cb6a33005b82019a41fd609a3d98dac
Morvai Barna <morvaibarna@gmail.com>
2023-04-24 16:50:12 +0200

    commenting

--- src/install.sh
+++ src/install.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-echo Install python
-sudo apt-get install python3
 
 # Set color for commands
 RED="\033[31m"
 NORMAL="\033[0;39m"
 
+# Install python
+echo Installing python
+sudo apt-get install python3
+
+
 # Set global environment variable for the script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
+
+# Rename the python files for easier access
 mv encrypt_file.py encrypt
 chmod +x encrypt
 
