68a7c4510c4b6dae965cb94c7c9d2aed7986aae4
Morvai Barna <morvaibarna@gmail.com>
2023-05-18 10:30:36 +0200

    added option to update

--- src/install.sh
+++ src/install.sh
@@ -5,32 +5,40 @@ RED="\033[31m"
 NORMAL="\033[0;39m"
 
 # Update system before installing
-sudo apt-get -y update
-sudo apt-get -y upgrade
+read -p "I recommend to update your system before installing dependencies. Do you want to update the OS? (y/n) " yn
+case $yn in
+    [yY] ) echo Ok, proceeding;
+        sudo apt-get -y update
+        sudo apt-get -y upgrade
+        break;;
+    [nN] ) echo Exiting...;
+        # Install python
+        echo Installing python
+        sudo apt-get install python3
 
-# Install python
-echo Installing python
-sudo apt-get install python3
 
+        # Set global environment variable for the script
+        DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+        echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
 
-# Set global environment variable for the script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-echo export PATH=$DIR:\$PATH >> $HOME/.bashrc
+        # Rename the python files for easier access
+        mv encrypt_file.py encrypt
+        chmod +x encrypt
 
-# Rename the python files for easier access
-mv encrypt_file.py encrypt
-chmod +x encrypt
+        mv zipper.py compress
+        chmod +x compress
 
-mv zipper.py compress
-chmod +x compress
+        # Check if pip is installed, and install it if necessary
+        if ! command -v pip &> /dev/null; then
+            echo -e $RED "Installing pip..." $NORMAL
+            sudo apt-get update
+            sudo apt-get install -y python3-pip
+        fi
 
-# Check if pip is installed, and install it if necessary
-if ! command -v pip &> /dev/null; then
-    echo -e $RED "Installing pip..." $NORMAL
-    sudo apt-get update
-    sudo apt-get install -y python3-pip
-fi
-
-# Install packages from requirements.txt
-echo -e $RED "Installing required packages..." $NORMAL
-pip install -r requirements.txt
+        # Install packages from requirements.txt
+        echo -e $RED "Installing required packages..." $NORMAL
+        pip install -r requirements.txt
+        break;;
+    * ) echo invalid response;;
+esac
+done
